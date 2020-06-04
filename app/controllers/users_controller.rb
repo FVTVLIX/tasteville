b0f0da1485fd5b87c5baff5d650e521284507686
FVTVLIX <andman290@gmail.com>
2020-06-04 15:25:46 -0400

    added authentication login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      @token = encode({user_id: @user.id, username: @user.username});
-      render json: {user: @user, token: @token}, status: :created, location: @user
+      @token = encode({user_id: @user.id});
+      render json: { user: @user, token: @token }, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
